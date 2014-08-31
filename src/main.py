7fbf68a07d8cded39e5bec47375db2e57e334cba
metaperl <metaperl@gmail.com>
2014-08-31 09:42:39 -0400

    cleanup

--- src/main.py
+++ src/main.py
@@ -52,21 +52,6 @@ ten_minutes   = 10 * one_minute
 one_hour      = 3600
 
 
-
-def hours_as_string(hours):
-    if hours:
-        return str(hours)
-    else:
-        return "not specified"
-
-def session_as_string(i):
-    if i < 0:
-        return "No Limit"
-    else:
-        return str(i)
-
-
-
 def url_for_action(action):
     return "{0}/{1}".format(base_url, action_path[action])
 
@@ -80,36 +65,6 @@ def wait_visible(driver, locator, by=By.XPATH, timeout=30):
     except TimeoutException:
         return False
 
-
-def try_method(fn):
-    @wraps(fn)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return fn(self, *args, **kwargs)
-        except:
-            print(traceback.format_exc())
-            while True:
-                pass
-
-    return wrapper
-
-# --- a "class" of maint window functions
-
-
-def current_time():
-    now = datetime.datetime.now()
-    return now.strftime("%I:%M%p")
-
-
-def current_time_log_format():
-    return "[ {0} ]".format(current_time())
-
-
-
-def my_time(dt):
-    return dt.strftime('%I:%M%p')
-
-
 class Entry(object):
 
     def __init__(
@@ -120,31 +75,6 @@ class Entry(object):
         self._second_password = second_password
         self.browser = browser
         self.action = action
-
-    def run_stats(self):
-        s = """
-Sess #\tSteps
-------------------
-"""
-        print(s)
-        for i, step in enumerate(self.steps, start=1):
-            print("{0}\t{1}".format(i, step))
-
-        c = Counter(self.steps)
-
-        print("""
-Total number of sessions {0}
-Mininum number of steps: {1}
-Maximum number of steps: {2}
-Average number of steps: {3}
-""".format(
-    len(self.steps), min(self.steps), max(self.steps), numpy.mean(self.steps)))
-
-        s = """Steps\t# of sessions with this step
---------------------------------------------"""
-        print(s)
-        for t in c.most_common():
-            print("{0}\t{1}".format(*t))
 
     def login(self):
         print("Logging in...")
@@ -161,17 +91,12 @@ Average number of steps: {3}
 
         self.browser.find_by_value('Login').first.click()
 
-
-
     def view_ads(self):
         for i in xrange(1,11):
             while True:
                 result = self.view_ad()
                 if result == 0:
                     continue
-
-
-
 
     def view_ad(self):
 
@@ -186,7 +111,7 @@ Average number of steps: {3}
 
         #print("Found {0} buttons".format(buttons))
 
-        buttons[random.randrange(0,5)].click()
+        buttons[0].click()
 
         self.solve_captcha()
 
@@ -194,7 +119,6 @@ Average number of steps: {3}
             time.sleep(1)
 
         return 0
-
 
     def calc_time(self):
 
@@ -223,7 +147,6 @@ Average number of steps: {3}
 
         loop_forever()
 
-
     def solve_captcha(self):
         time.sleep(3)
         elem = self.browser.find_by_xpath(
@@ -241,198 +164,6 @@ Average number of steps: {3}
         button = self.browser.find_by_name('Submit')
         button.click()
 
-
-    def choose_direction(self):
-        #time.sleep(6)
-        lookup = '//*[@class="bet_button {0} button"]'.format(self.direction)
-
-        button = wait_visible(self.browser.driver, lookup)
-        if button:
-            button.click()
-        else:
-            raise("Cannot locate direction button to click")
-
-
-        if self._trending:
-            print("Trending: {0}".format(self._trending))
-            print("Current direction:{0}.".format(self.direction))
-            if self.direction == 'lower':
-                self.direction = 'higher'
-            elif self.direction == 'higher':
-                self.direction = 'lower'
-            print("New direction:{0}".format(self.direction))
-
-    def input_stake(self, stake_i, amount):
-        print("{0} Stake {1} = ${2} ".format(
-            current_time_log_format(), stake_i, amount))
-        self.browser.fill('amount', str(amount))
-
-    def buy_button_value(self):
-        button = self.browser.find_by_name('buy').first
-        return button.value
-
-    def wait_for_enabled_buy_button(self):
-        while True:
-            if self.buy_button_value() == 'BUY':
-                break
-            else:
-                time.sleep(1)
-
-    def buy(self):
-        self.wait_for_enabled_buy_button()
-        button = self.browser.find_by_name('buy').first
-        #print 'Buy button', button
-        button.click()
-
-    def won(self, result):
-        return "WON" in result
-
-    def trade_result(self):
-        """1 = win
-        0 = tie
-        -1 = lost"""
-        table = self.browser.find_by_id('completed_investments')
-        tbody = table.find_by_tag('tbody')
-        tr = tbody.find_by_tag('tr').first
-        result = tr['class']
-        if "PUSH" in result:
-            return 0
-        elif "WON" in result:
-            return 1
-        elif "LOST" in result:
-            return -1
-        else:
-            print(result, "is not recognized")
-            sys.exit(1)
-
-    def active_trades(self):
-        active_investments_table = self.browser.find_by_xpath(
-            '//table[@id="active_investments"]')
-        tbody = active_investments_table.find_by_tag('tbody').first
-        try:
-            if (tbody.find_by_tag('td').first)['class'] == 'dataTables_empty':
-                return 0
-            else:
-                return 1
-        except:
-            print("\tsome error checking status of datatable. returning 0")
-            return 0
-
-    def show_progress_til_expiry(self, date_td):
-        date_td
-        date = date_td.value  # Apr 22, 19:25:00
-        dt = datetime.strptime(date, '%b %d, %H:%M:%S')
-        n = datetime.now()
-        dt = dt.replace(n.year)
-
-        diff = dt - n
-        wait_time = int(round(diff.total_seconds()))
-
-        for i in progress.bar(range(wait_time)):
-            time.sleep(1)
-
-    def wait_until_active_trade_is_completed(self, expiry_date_string):
-        while True:
-            time.sleep(1)
-            try:
-                table = self.browser.find_by_id('completed_investments')
-                tbody = table.find_by_tag('tbody')
-                latest_completed_trade_string = tbody.find_by_tag('td').first.value
-                #print("Latest completed trade date: {0}. Expiry_date_string: {1}.", latest_completed_trade_string, expiry_date_string)
-                if latest_completed_trade_string == expiry_date_string:
-                    break
-            except e:
-                print("Error reading DOM: {1}".format(e.strerror))
-                continue
-
-
-    def wait_for_active_trade_to_finish(self):
-        time.sleep(10)
-        active_investments_table = self.browser.find_by_xpath('//table[@id="active_investments"]')
-        tbody = active_investments_table.find_by_tag('tbody').first
-        tds = tbody.find_by_tag('td')
-
-        expiry_date_string = tds[0].value
-        self.show_progress_til_expiry(tds[5])
-
-        self.wait_until_active_trade_is_completed(expiry_date_string)
-
-
-
-
-    def poll_for_active_trades(self):
-        while True:
-            if not self.active_trades():
-                return
-            else:
-                time.sleep(10)
-
-    def _trade(self, stake_i, stake):
-        self.choose_direction()
-        self.input_stake(stake_i, stake)
-        self.buy()
-
-        self.wait_for_active_trade_to_finish()
-
-        #print("\t ", end="")
-
-        if self.trade_result() > 0:
-
-            print("* ITM *")
-            return 1
-        elif self.trade_result() < 0:
-            print("OTM")
-            return -1
-        else:
-            print("Draw.")
-            return self._trade(stake_i, stake)
-
-
-    def intersession_break(self, i):
-        rejoice = random.randint(60,90)
-        notice = """
-Session {0}/{1} completed. Pausing for {2} seconds.
-{3}
-========================================================
-"""
-        print(notice.format(i, session_as_string(self.sessions), rejoice, self.timer.status()))
-        time.sleep(rejoice)
-
-
-    @try_method
-    def trade(self, seq, iterate=True):
-
-        for i, stake in enumerate(seq, start=1):
-            result = self._trade(i, stake)
-            if result > 0:
-                self.steps.append(i)
-                break
-
-
-    def tradeloop(self, session_number, s, ignore_window):
-
-        if not ignore_window:
-            self.check_for_maintenance_window()
-        self.trade(s)
-
-        self.intersession_break(session_number)
-
-    def check_for_maintenance_window(self, entered=False):
-        if in_maintenance_window():
-            notice = "{0} Halting trading: in/near maintenance window of {1} to {2}"
-            print(notice.format(
-                current_time_log_format(),
-                my_time(maintenance_window['start']),
-                my_time(maintenance_window['finish'])
-                )
-            )
-            time.sleep(three_minutes)
-            return self.check_for_maintenance_window(True)
-        else:
-            return entered
-
-
-
 def main(username, password, second_password,random_delay=False,action='click'):
 
     if random_delay:
@@ -445,9 +176,7 @@ def main(username, password, second_password,random_delay=False,action='click'):
 
         browser.driver.set_window_size(1200,1100)
 
-
-        e = Entry(username, password, second_password, browser, action
-              )
+        e = Entry(username, password, second_password, browser, action)
 
         e.login()
 
